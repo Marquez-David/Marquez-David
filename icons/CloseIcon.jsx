6e866f8a87f058dc486f6ab7043b18871eaad140
David Márquez <45391647+Marquez-David@users.noreply.github.com>
2025-03-23 16:42:16 +0100

    Refactor experience data structure and update Card and Modal components; add CloseIcon and replace StepperItem with Item component

---
+++ icons/CloseIcon.jsx
@@ -0,0 +1,16 @@
+const CloseIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill=""
+    viewBox="0 0 24 26"
+    strokeWidth={2}
+    stroke="currentColor"
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+  </svg>
+);
+
+
+export default CloseIcon;
+
